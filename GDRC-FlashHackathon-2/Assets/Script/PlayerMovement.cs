c5b2f2f31fb77e289f650a7589b9b96de00093e2
paopeaw <paopeaw8246@gmail.com>
2023-08-12 15:41:36 +0700

    Update PlayerMovement.cs

--- GDRC-FlashHackathon-2/Assets/Script/PlayerMovement.cs
+++ GDRC-FlashHackathon-2/Assets/Script/PlayerMovement.cs
@@ -1,8 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float rotateSpeed;
+    public PlayerInput playerinput;
+
+    private InputAction move; 
+    private Vector2 moveDir;
+
+    private void Awake() {
+        rb = this.GetComponent<Rigidbody2D>();
+        playerinput = new PlayerInput();
+    }
+
+    private void OnEnable() {
+        move = playerinput.Player.Move;
+        move.Enable();
+    }
+    
+    private void OnDisable() {
+        move.Disable();
+    }
+
+
+    private void Update() {
+        moveDir = move.ReadValue<Vector2>();
+
+    }
+
+    private void FixedUpdate() {
+        var impulse = (-moveDir.x * rotateSpeed * Mathf.Deg2Rad) * rb.inertia;
+        rb.AddTorque(impulse); 
+        rb.AddForce(this.transform.right * moveDir.y * moveSpeed);
+        
+    }
+
 
 }

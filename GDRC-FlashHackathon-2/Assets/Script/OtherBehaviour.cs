ed6e1fe65d4c2f91d393c25d3ef0435daf5ec3f9
paopeaw <paopeaw8246@gmail.com>
2023-08-13 03:03:28 +0700

    dialogue + dotween

--- GDRC-FlashHackathon-2/Assets/Script/OtherBehaviour.cs
+++ GDRC-FlashHackathon-2/Assets/Script/OtherBehaviour.cs
@@ -6,16 +6,21 @@ using UnityEngine.Rendering.Universal.Internal;
 public class OtherBehaviour : MonoBehaviour
 {
     public string Name;
+    [SerializeField] private float rotationSpeed = 1;
 
     public void StartInteract(GameObject player){
         this.GetComponent<OtherRandomMovement>().enabled = false;
         FaceToPlayer(player.transform);
+
+        DialougeManger.instance.StartFriendDialouge(player, this.gameObject);
     }
 
     private void FaceToPlayer(Transform target){
         Vector3 direction = target.position - transform.position;
-        Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
-        transform.rotation = rotation;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        var targetRotation = Quaternion.Euler(0f, 0f, angle);
+        transform.rotation = targetRotation;
+        // transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 
 }
